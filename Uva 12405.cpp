cc9105124592b2dc9227237890af37240a82b1c1
FelipeCoCla <42448123+FelipeCoCla@users.noreply.github.com>
2018-11-15 00:08:44 -0300

    Create Uva 12405.cpp

---
+++ Uva 12405.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+char s;
+vector <bool> campo;
+vector <string> str;
+int n,espantapajaros,m,postrue;
+int contador = 0;
+
+int main()
+{
+    cin >> n;
+    while(n--){
+        cin >> m;
+        contador += 1;
+        espantapajaros = 0;
+
+        for (int i=0;i<m;i++){
+            cin >> s;
+            s;
+            if (s == '.'){
+                campo.push_back(true);
+                postrue = i;
+            } else {
+                campo.push_back(false);
+            }
+        }
+
+        for (int i=1;i<=campo.size();i++){
+            if (campo[i-1] == true) {
+                espantapajaros += 1;
+                campo [i-1] = false;
+                campo[i] = false;
+                campo[i+1] = false;
+            }
+        }
+        cout << "Case " << contador << ": " << espantapajaros << endl;
+    }
+
+    return 0;
+}
