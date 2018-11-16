d8d988124b996427c836efe6706db2c4cb1b74cb
FelipeCoCla <42448123+FelipeCoCla@users.noreply.github.com>
2018-11-15 23:46:19 -0300

    Update *Uva 11572.cpp

--- *Uva 11572.cpp
+++ *Uva 11572.cpp
@@ -13,17 +13,17 @@ int main(){
 	while( t-- ){
 		scanf( "%d", &n );
 		m.clear();
-		mx = u = 0; 
+		mx = u = 0; //reinicia las variable y el vector m para cada caso 
 		for( i=1; i<=n; ++i ){
 			scanf( "%d", &x );
 			int& r = m[x]; 
 			if( r>u ){
-				mx = max(mx,i-u-1);
-				u = r;
+				mx = max(mx,i-u-1); //calculo el maximo entre la varible maximo y el numero de posiciones i-u-1
+				u = r; // el u ar la siguiente iteracion se convierte en r
 			}
-			r = i;
+			r = i; // el rango de a proxima iteracion partir en la posicion i, ojo ++i
 		}
-		mx = max(mx,n-u);
+		mx = max(mx,n-u); //maximo entre el mx calculado y el numero de posiciones el vector menos el ultimo u = r
 		printf( "%d\n", mx );
 	}
 }
